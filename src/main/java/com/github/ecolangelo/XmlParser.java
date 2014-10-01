8c57f87b1bb972908ab8b3d8be97df9a99845d22
Eros Colangelo <eros@Eross-MacBook-Pro.local>
2014-10-01 20:15:03 +0200

    add inputStream in the signature of XmlParser

--- src/main/java/com/github/ecolangelo/XmlParser.java
+++ src/main/java/com/github/ecolangelo/XmlParser.java
@@ -1,6 +1,7 @@
 package com.github.ecolangelo;
 
 import javax.xml.stream.XMLStreamException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.Map;
 
@@ -8,7 +9,7 @@ import java.util.Map;
  * Created by eros on 06/09/14.
  */
 public interface XmlParser {
+    public Map<String,String> parse(InputStream inputStream) throws XMLStreamException;
 
     public Map<String,String> parse(String xml, Charset charset) throws XMLStreamException;
-
 }
